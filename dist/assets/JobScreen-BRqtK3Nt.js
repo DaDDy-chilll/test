b0e95de5be822e16b9af71c8f896a42aaab6e3f9
Who <blacklistuser420@gmail.com>
2024-09-26 19:37:20 +0630

    update

---
+++ dist/assets/JobScreen-BRqtK3Nt.js
@@ -0,0 +1,1 @@
+import{E as e,ab as t}from"./index-NtpdcA5x.js";const n=()=>e.jsx(t,{});export{n as default};
--- dist/assets/queryKey-UAMLCRrd.js
+++
@@ -1,1 +0,0 @@
-var s=(a=>(a.APPLICANTS="applicants",a.APPLICANT_DETAIL="applicantDetail",a.JOBS="jobs",a.USERS="users",a.EVENTS="events",a))(s||{});export{s as Q};
